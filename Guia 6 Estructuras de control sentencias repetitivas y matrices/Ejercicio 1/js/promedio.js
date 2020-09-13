c9c7efd9ed045ec8d06eced93662890b2b24cca1
C3SC0-V4113 <C3SC0-V4113@users.noreply.github.com>
2020-09-13 11:48:09 -0600

    Actualizacion pequeña

--- Guia 6 Estructuras de control sentencias repetitivas y matrices/Ejercicio 1/js/promedio.js
+++ Guia 6 Estructuras de control sentencias repetitivas y matrices/Ejercicio 1/js/promedio.js
@@ -13,13 +13,12 @@ document.write("<section>");
 document.write("<article>");
 for (i=1;i<=num;i++){
  notaactual = parseFloat(prompt("Ingrese la nota " + i + ": ",""));
-document.write("<h3>La nota " + i + ": " + Math.round(notaactual * Math.pow(10,2))/Math.pow(10,2) + "</h3>");
+ document.write("<h3>La nota " + i + ": " + Math.round(notaactual * Math.pow(10,2))/Math.pow(10,2) + "</h3>");
  nota += notaactual;
 }
 //Obteniendo el cálculo del promedio
 promedio = nota / (i-1);
 //Imprimir el valor redondeado a dos decimales del promedio
-document.write("<h3>El promedio de las notas es: " + Math.round(promedio * Math.pow(10,2))
-/ Math.pow(10,2) + "</h3>");
+document.write("<h3>El promedio de las notas es: " + Math.round(promedio * Math.pow(10,2))/ Math.pow(10,2) + "</h3>");
 document.write("</article>");
 document.write("</section>");
