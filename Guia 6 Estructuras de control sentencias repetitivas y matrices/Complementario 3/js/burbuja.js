f0234412b711b05286bffbe71e06c57b2fcf074e
C3SC0-V4113 <C3SC0-V4113@users.noreply.github.com>
2020-09-14 15:36:07 -0600

    Terminado Ejercicios Complementarios Guia 6 y Mejorado Ejercicio 2 del primer Parcial Practico

--- Guia 6 Estructuras de control sentencias repetitivas y matrices/Complementario 3/js/burbuja.js
+++ Guia 6 Estructuras de control sentencias repetitivas y matrices/Complementario 3/js/burbuja.js
@@ -2,22 +2,23 @@ function inicializar(){
     //Inicializacion de variables
     var numeros=new Array();
     var i, max,temp,contenido="",tdelement,ordenamiento;
-    var ORDEN;
+    var ORDEN=0;
     /*Validacion para que el número de elementos del arreglo sea
     numérico y mayor o igual a 2 */
     do{
         max=prompt('Cuántos número va a ingresar (Valor entero) :','');
         ordenamiento=prompt('Cual será el orden de los datos ("ASC" para Ascendente y "DESC" para Descendente)','');
-        ORDEN=ordenamiento.toUpperCase();
-        switch (ORDEN) {
-            case ASC:
-                console.log("Ascendente papu");
+        ORDEN=0;
+        //Verificar el tipo de Ordenamiento
+        switch (ordenamiento.toUpperCase()) {
+            case 'ASC':
+                ORDEN=1;
                 break;
-            case DESC:
-                console.log("Descendente papu");
+            case 'DESC':
+                ORDEN=2;
                 break;
             default:
-                console.log("¿tas tonto papu?");
+                alert('Se debe escoger un orden válido');
                 break;
         }
         //Verificar que se ingrese un dato numérico
@@ -29,11 +30,18 @@ function inicializar(){
         if(max<2){
             alert('El arreglo debe ser de dimensión 2 o superior');
         }
-    }while(isNaN(max)||max<2);
+    }while(isNaN(max)||max<2||ORDEN==0);
 
     //Lazo para solicitar el ingreso de los elementos del arreglo
     for (i = 0; i < max; i++) {
-        numeros[i]=parseInt(prompt("Número "+(parseInt(i)+1),""));
+        num=prompt("Número "+(parseInt(i)+1),"");
+        if(isNaN(num)){
+            numeros[i]=0;
+        }
+        else{
+            numeros[i]=parseInt(num);
+        }
+        /*numeros[i]=parseInt(prompt("Número "+(parseInt(i)+1),""));*/
     }
     /*Obteniendo el elemento donde se cargará el contenido
     generado dinámicamente desde JavaScript */
@@ -47,17 +55,32 @@ function inicializar(){
             contenido += "\t\t\t<td class=\"Off\">" + numeros[i] + "</td>\n";            
         }
         contenido += "\t\t</tr>\n\t</tbody>\n</table>\n<br />\n\n";
-        //Lazo que ordena el arreglo mediante el método de la burbuja
-        for (i = 0; i < max-1; i++) {
-            for (let j = i+1; j < max; j++) {
-                if(numeros[i]>numeros[j]){
-                    temp=numeros[j];
-                    numeros[j]=numeros[i];
-                    numeros[i]=temp;
-                }                
-            }            
+        if(ORDEN==1){
+            //Lazo que ordena el arreglo mediante el método de la burbuja
+            for (i = 0; i < max-1; i++) {
+                for (let j = i+1; j < max; j++) {
+                    if(numeros[i]>numeros[j]){
+                        temp=numeros[j];
+                        numeros[j]=numeros[i];
+                        numeros[i]=temp;
+                    }                
+                }            
+            }
+            contenido += "<h1>Números ordenados ascendentemente</h1>\n";
         }
-        contenido += "<h1>Números ordenados ascendentemente</h1>\n";
+        else{
+            //Lazo que ordena el arreglo mediante el método de la burbuja
+            for (i = 0; i < max-1; i++) {
+                for (let j = i+1; j < max; j++) {
+                    if(numeros[i]<numeros[j]){
+                        temp=numeros[j];
+                        numeros[j]=numeros[i];
+                        numeros[i]=temp;
+                    }                
+                }            
+            }
+            contenido += "<h1>Números ordenados descendentemente</h1>\n";
+        }
         contenido += "<table>\n\t<tbody>\n\t\t<tr>\n";
         /*Lazo que muestra los elementos del arreglo que han sido ordenados
         con el metodo de burbuja */
@@ -69,7 +92,6 @@ function inicializar(){
     datos.innerHTML=contenido;
     //Capturando los elementos con clase Off
     tdelement=document.getElementsByClassName('Off');
-    alert(tdelement.length);
     for (var i = 0; i < tdelement.length; i++) {
         tdelement[i].onmouseover=function(){
             this.className='On';
